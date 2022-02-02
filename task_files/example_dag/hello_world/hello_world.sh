68c5d3cc0b93dec6d057f315f1f57e63b9b776bc
Robert Gutierrez <gutierrez@carnegiehub.org>
2022-02-01 19:45:46 -0800

    fix linting, fix bugs

--- task_files/example_dag/hello_world/hello_world.sh
+++ task_files/example_dag/hello_world/hello_world.sh
@@ -4,12 +4,16 @@ source /root/.bashrc
 cd /root
 
 # copy task files
-gsutil -m -q cp -r -n gs://$CLOUD_STORAGE_BUCKET/task_files/example_dag/hello_world/** .
-chmod +x ./hello_world.R
+echo "Copying task files..."
+gsutil -m -q cp -r -n gs://$TASK_FOLDER/** .
 chmod +x ./hello_world.Rmd
+chmod +x ./hello_world.R
+echo "Done."
 
 # copy common files
+echo "Copying common files..."
 gsutil -m -q cp -r gs://$CLOUD_STORAGE_BUCKET/common_files/* .
+echo "Done."
 ls -al .
 
 # task code
@@ -19,5 +23,5 @@ Rscript -e "options(tinytex.verbose = TRUE); rmarkdown::render('hello_world.Rmd'
 touch ./hello_world.csv
 echo "Name,Message" >> hello_world.csv
 echo "$INSTANTIATOR,Hello world" >> hello_world.csv
-gsutil cp hello_world.csv gs://$CLOUD_STORAGE_BUCKET/runs/example_dag/$CF_RUN_ID/hello_world/
-gsutil cp hello_world.pdf gs://$CLOUD_STORAGE_BUCKET/runs/example_dag/$CF_RUN_ID/hello_world/
+gsutil cp hello_world.csv gs://$CLOUD_STORAGE_BUCKET/runs/example_dag/$CUSTOM_RUN_ID/hello_world/
+gsutil cp hello_world.pdf gs://$CLOUD_STORAGE_BUCKET/runs/example_dag/$CUSTOM_RUN_ID/hello_world/
